6b72fe4d6dd12980c3c220e1061901f80e519887
Egor Shvab <egor.shvab.537@gmail.com>
2020-04-23 17:42:44 +0300

    rework login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,16 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-<!--
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--->
     <link rel="stylesheet" href="{{ asset('application/fonts/style.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/jquery.scrollbar.css') }}">
@@ -25,55 +16,6 @@
 </head>
 <body>
 <div id="app">
-<!-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <ul class="navbar-nav ml-auto">
-                        @guest
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-@else
-    <li class="nav-item dropdown">
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-{{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-        </form>
-    </div>
-</li>
-@endguest
-    </ul>
-</div>
-</div>
-</nav>
--->
 
     <header class="header" id="header">
         <div class="container">
@@ -90,11 +32,11 @@
                         @guest
                             <ul class="auth-menu">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Вход') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ 'Вход' }}</a>
                                 </li>
                                 @if (Route::has('register'))
                                     <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+                                        <a class="nav-link" href="{{ route('register') }}">{{ 'Регистрация' }}</a>
                                     </li>
                                 @endif
                             </ul>
@@ -119,7 +61,6 @@
                                 @csrf
                             </form>
                         @endguest
-
                     </div>
                 </div>
             </div>
@@ -127,10 +68,8 @@
     </header>
 
     <div class="content scrollbar-macosx" id="container">
-
         <div class="container">
             <div class="content__inner">
-
                 <main class="main">
                     @yield('content')
                 </main>
@@ -139,8 +78,6 @@
     </div>
 
 </div>
-
-<script src="{{ asset('application/js/main.js') }}"></script>
 
 </body>
 </html>
